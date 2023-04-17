e244b3fb747429832dd4a299f74ada739121b20f
PierreGoupil13 <p.goupil356@gmail.com>
2023-04-17 14:14:20 +0200

    ok

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,12 +5,12 @@ function Navbar() {
         <nav className="navBar">
             <div className="navBar-list">
                 <div className="navBar-nested">
-                    <div className="navBar-list-item"><a className="navbar-hyper-item">Home</a></div>
-                    <div className="navBar-list-item"><a className="navbar-hyper-item">Projects</a></div>
+                    <div className="navBar-list-item"><a href="#" className="navbar-hyper-item">Home</a></div>
+                    <div className="navBar-list-item"><a href="#" className="navbar-hyper-item">Projects</a></div>
                 </div>
                 <div className="navBar-nested">
-                    <div className="navBar-list-item"><a className="navbar-hyper-item">About</a></div>
-                    <div className="navBar-list-item"><a className="navbar-hyper-item">Contact</a></div>
+                    <div className="navBar-list-item"><a href="#" className="navbar-hyper-item">About</a></div>
+                    <div className="navBar-list-item"><a href="#" className="navbar-hyper-item">Contact</a></div>
                 </div>
             </div>
         </nav>
